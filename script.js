cb88a27d36c0ccc0c6804e32c528a9ac9b4227fe
jellyflsh <eya.kaddour@hotmail.fr>
2025-01-01 19:05:51 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.totoro').forEach(totoro => {
-    const randomDuration = Math.random() * 2 + 3; // Durée entre 3 et 5 secondes
-    const randomDelay = Math.random() * 2; // Délai entre 0 et 2 secondes
-    totoro.style.animationDuration = `${randomDuration}s`;
-    totoro.style.animationDelay = `${randomDelay}s`;
-});
